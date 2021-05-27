4d732376b740ee6e8480ed8f3aaed9c84c4efab2
William-arno CLÉMENT <wiyam@icloud.com>
2021-05-27 08:39:28 +0200

    quelques ajustements

--- components/app.php
+++ components/app.php
@@ -11,18 +11,31 @@ $crawler = get_app_info($cle_app);
   <h2><?php echo $crawler['nom']; ?>  <button class="mdl-button mdl-js-button mdl-button--fab mdl-color--orange-100 dialog-button" >
     <i class="material-icons">create</i></h2>
   </button>
+  <h3>Description</h3>
   <p><?php echo $crawler['description']; ?></p>
   <?php if ($_SESSION &&($_SESSION['isAdmin'] == true || $_SESSION['isTech'] == true)): ?>
+    <h3>Fiche technique</h3>
     <p>Emplacement du logiciel: <a href="<?php echo $crawler['emplacement']; ?>"><?php echo $crawler['emplacement']; ?></a></p>
     <p>Emplacement de la procédure: <a href="<?php echo $crawler['procedure_']; ?>"><?php echo $crawler['procedure_']; ?></a></p>
   <?php endif; ?>
   <?php $crawl_editeur = get_editeur_name($crawler['cle_editeur']); ?>
+  <h3>Informations</h3>
   <p>Le logiciel est édité par <b>l'éditeur</b> suivant: <a href="<?php echo 'editeur.php?editeur='.$crawl_editeur['cle']; ?>"><?php echo $crawl_editeur['nom']; ?></a></p>
   <p>Le logiciel est utilisé dans les <b>directions</b> suivantes:</p>
   <?php require 'frameworks/WAC_Lic_functions.php'; ?>
   <?php foreach (get_dir_from_app($crawler['cle']) as $k): ?>
     <p style="color:blue;"><?php echo $k['nom']; ?></p>
   <?php endforeach; ?>
+  <?php require 'frameworks/WAC_GetBudgetFromCleLogiciel.php'; ?>
+  <?php $k = get_budget_from_cle_logiciel($crawler['cle']); ?>
+  <?php if (isset($k)): ?>
+    <h3>Fiche budget</h3>
+    <p>Le cout par an de la licence logiciel s'élève à <b><?php echo $k['coutParAn']; ?> euros.</b></p>
+  <?php endif; ?>
+  <?php if (!isset($k)): ?>
+    <h3>Fiche budget (non complétée)</h3>
+    <p>Le cout de la licence logiciel n'est pas déterminée.</p>
+  <?php endif; ?>
 </div>
 <dialog id="dialog" class="mdl-dialog">
   <h3 class="mdl-dialog__title">Modifier une application existante</h3>
